a15ff479b3501509f317ff0e4365964fa94c723e
MediocreDev <liammaloneirlusa@gmail.com>
2023-11-11 16:52:44 +0000

    fixed binary search

--- search/binary_search.zig
+++ search/binary_search.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
+const testing = std.testing;
 
 fn binsearch(key: i32, arr: []i32) usize {
     var lo: usize = 0;
     var hi: usize = arr.len - 1;
-    while (hi <= lo) {
+    while (lo <= hi) {
         const mid = lo + (hi - lo) / 2;
         if (key == arr[mid]) return mid;
         if (key > arr[lo]) {
@@ -15,7 +16,7 @@ fn binsearch(key: i32, arr: []i32) usize {
     return arr.len + 1;
 }
 
-pub fn main() void {
+test "find number 5 in array 1-7 -- should give index 4" {
     var arr = [_]i32{ 1, 2, 3, 4, 5, 6, 7 };
-    std.debug.print("index of {d}, is : {d}\n", .{ 5, binsearch(5, &arr) });
+    try testing.expectEqual(@as(usize, 4), binsearch(5, &arr));
 }
