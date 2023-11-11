d2cef2a888e9f81f85990359b14a63721574a6f8
MediocreDev <liammaloneirlusa@gmail.com>
2023-11-11 16:18:17 +0000

    refactored to take cli input

--- hanoi/src/main.zig
+++ hanoi/src/main.zig
@@ -1,19 +1,35 @@
 const std = @import("std");
 
-fn hanoi(n: i32, from: u8, to: u8, aux: u8) !void {
-    if (n == 0){
+fn hanoi(n: usize, from: u8, to: u8, aux: u8) !void {
+    if (n == 0) {
         return;
     }
-    try hanoi(n-1, from, aux, to);
-    std.debug.print("Move disk {}, from {c}, to {c}\n", .{n, from, to});
-    try hanoi(n-1, aux, to, from);
+    try hanoi(n - 1, from, aux, to);
+    std.debug.print("Move disk {}, from {c}, to {c}\n", .{ n, from, to });
+    try hanoi(n - 1, aux, to, from);
 }
 
 pub fn main() !void {
-    // add in CLI arg parsing to run this with any size tower
-    //var args = std.process.argsWithAllocator();
-    //const n = args.next();
-    //std.debug.print("{any}", .{n});
-    std.debug.print("Solving tower of hanoi with a size of 3\n\n", .{});
-    try hanoi(3, 'A', 'B', 'C');
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    var n: usize = 3;
+    const stderr = std.io.getStdErr().writer();
+
+    if (args.len < 2) {
+        try stderr.print("Error: no tower height provided\n", .{});
+        std.process.exit(1);
+    } else {
+        n = try std.fmt.parseInt(usize, args[1], 10);
+    }
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Solving tower of hanoi with a size of {d}\n\n", .{n});
+
+    try hanoi(n, 'A', 'B', 'C');
 }
